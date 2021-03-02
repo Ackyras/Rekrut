3af9563fa90cabed10df77e9f9c69b9e7c3b86b3
ackyras <ackyrasibarani@gmail.com>
2021-03-02 11:35:12 +0700

    matakuliah DB and Model

---
+++ database/migrations/2021_03_01_062017_create_matakuliahs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatakuliahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matakuliahs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('nama');
+            $table->string('dosen');
+            $table->date('tanggalseleksi');
+            $table->time('jamseleksi');
+            $table->time('akhirseleksi');
+            $table->integer('idpembukaan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matakuliahs');
+    }
+}
