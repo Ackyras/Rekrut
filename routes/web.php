2ade20ab3c01dc4e82e6eb62ed7bbb1e5ae1a706
ackyras <ackyrasibarani@gmail.com>
2021-03-02 11:36:15 +0700

    route 30

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\pendaftarcontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Pembukaan pendaftaran by admin
+Route::get('/open-pendaftaran', [pendaftarcontroller::class, 'openpendaftaran'])->name('pembukaan');
+Route::post('/open-pendaftaran', [pendaftarcontroller::class, 'prosesopen'])->name('prosespembukaan');
+Route::get('/open-pendaftaran/tambah', [pendaftarcontroller::class, 'tambahpendaftaran'])->name('tambahpendaftaran');
+Route::post('/open-pendaftaran/tambah', [pendaftarcontroller::class, 'prosestambahpendaftaran'])->name('prosestambahpendaftaran');
+
+// Penambahan matakuliah
+Route::get('/mata-kuliah/{id}',[pendaftarcontroller::class, 'tambahmatkul'])->name('tambahmatkul');
+Route::post('/mata-kuliah/{id}',[pendaftarcontroller::class, 'prosestambahmatkul'])->name('prosestambahmatkul');
+
+// main admin
+Route::get('/admin', [admincontroller::class, 'index'])->name('homeadmin');
+Route::get('/soal-generator', [])->name('generatorsoal');
+
+// pendaftaran peserta
+Route::get('/daftar', [pendaftarcontroller::class, 'daftar'])->name('pendaftaran');
+Route::post('/daftar/proses', [pendaftarcontroller::class, 'proses'])->name('prosespendaftaran');
